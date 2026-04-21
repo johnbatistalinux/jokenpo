27e2fabcb33e8e632040c931809cb157c2dbe363
John Batista <98060258+johnbatistalinux@users.noreply.github.com>
2026-04-21 19:27:03 -0300

    Add files via upload

---
+++ jokenpo.py
@@ -0,0 +1,28 @@
+print("---------------escolha o modo de jogo---------------")
+print("1.humano vs humano-----",end="")
+print("2.humano vs maquina-----",end="")
+print("3.maquina vs maquina")
+
+
+while True:
+    escolhamodo=int(input(""))
+    print("1.tesoura----2.pedra----3.papel")
+    if escolhamodo==1:
+        print("jogador 1, faça sua jogada")
+        jogador1=int(input(""))
+        print("jogador 2, faça sua jogada")
+        jogador2=int(input(""))
+        
+
+
+
+        
+        
+
+
+
+
+
+
+        
+
